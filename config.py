43d0a6ee80bd9762731b468a9f47512e125346a4
klop101023-svg <klop101023@gmail.com>
2026-09-05 02:55:02 +1000

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 VK_TOKEN = os.getenv("VK_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL", "https://api.aitunnel.ru/v1")
-OPENAI_MODEL = os.getenv("OPENAI_MODEL", "qwen/qwen3-coder")
 
 # === GigaChat ===
 GIGACHAT_API_KEY = os.getenv("GIGACHAT_API_KEY")
