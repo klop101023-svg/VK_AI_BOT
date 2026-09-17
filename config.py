1f892aca9625562131cc4b3ced20cb108559ce38
klop101023-svg <klop101023@gmail.com>
2026-09-18 01:12:37 +1000

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# === ВКонтакте ===
 VK_TOKEN = os.getenv("VK_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
 
 # === GigaChat ===
-GIGACHAT_API_KEY = os.getenv("GIGACHAT_API_KEY")
+GIGACHAT_CREDENTIALS = os.getenv("GIGACHAT_CREDENTIALS")
 GIGACHAT_SCOPE = os.getenv("GIGACHAT_SCOPE", "GIGACHAT_API_PERS")
